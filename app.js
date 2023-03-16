2efd51631232457ba79f78b3cfec75df8f040a4f
Dmytro Hirich <diman12345677@gmail.com>
2023-03-16 12:21:18 +0200

    adding scripts // production to express

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const config = require("config");
+const path = require('path')
 const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json({ extended: true }));
 
-// connect routes
+// connect routes // creating api
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/link", require("./routes/link.routes"));
 app.use("/t", require("./routes/redirect.routes"));
+
+// handling builded frontend 
+if (process.env.NODE_ENV === 'production') {
+  // express static to point static front-end folder 'client'
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  // any get request
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index'))
+  })
+}
 
 const PORT = config.get("port") || 5000;
 
